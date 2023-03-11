0b0ecaa1f80045d7a500cca5ff7feb1ecac0cf0e
Kodiererin <iamujjwal45@gmail.com>
2023-03-12 02:49:18 +0530

    Addding Learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import { useEffect } from 'react';
 
-
+import { useState } from 'react';   // using State Hooks
+// Any function that starts with a use in react we call it a Hook
 
 const App = () => {
-
-  return (
+  const[counter , updateCounter] = useState(0);
+  useEffect(()=>{
+    // Use effect allows us to do something or some kind of a effect or some kind of an event.
+    // alert("Reloaded");
+    // Alert message appears in the page reload
     
+    // Never Modify state manually.
+    updateCounter(100);
+
+  },[])
+
+  // This array in the useEfffecr is the dependency array!
+  // which stores the changes done by the user.
+  // without the array the useEffect() will update and again the counter will reset back to 100.
+  function add(){
+    updateCounter(counter+1);
+  }
+  function sub(){
+    updateCounter(counter-1);
+  }
+  return (
+    <div className='App'>
+      <button onClick={sub}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={add}>+</button>
+    </div>
   );
 }
 
@@ -76,4 +101,32 @@ export default App;
 // {/* <Person name = {"ujjwal"} lName = {'Kumar'} age={21}/>
 // </div> */}
 
-// State : 
+// State : State in a react is a plain Javascript object used by React to represent a piece of information about
+// the components current situation. It is completely managed by the component itself.
+
+
+// Hooks : 
+// import { useState } from 'react';   // using State Hooks
+// // Any function that starts with a use in react we call it a Hook
+
+// Event : Event is an action that can be triggerered as a result of the user action or some kind of a system generated event.
+// eg : onClick or onHover is an event.
+
+// const App = () => {
+//   const[counter , updateCounter] = useState(0);
+//   function add(){
+//     updateCounter(counter+1);
+//   }
+//   function sub(){
+//     updateCounter(counter-1);
+//   }
+//   return (
+//     <div className='App'>
+//       <button onClick={sub}>-</button>
+//       <h1>{counter}</h1>
+//       <button onClick={add}>+</button>
+//     </div>
+//   );
+// }
+
+// See the Documentaiton of the Hooks!!!!!!!!.
