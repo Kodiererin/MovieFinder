b54b595fb717dcd365878080c260f8985a627819
Kodiererin <iamujjwal45@gmail.com>
2023-03-14 14:41:19 +0530

    Adding Files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { useEffect } from "react";
+import './'
 // http://www.omdbapi.com/?i=tt3896198&apikey=383193b6
 // const key = http://www.omdbapi.com/?i=tt3896198&apikey=
 const key = '383193b6';
-const apiURL = 'http://www.omdbapi.con?apikey='+key;
+const apiURL = 'http://www.omdbapi.com?apikey='+key;
 
 const App = () =>{
     const searchMovies = async(title)=>{
         const response = await fetch(`${apiURL}&s=${title}`)
         const data = await response.json();
-        console.log(data);
+        console.log(data.Search);
     }
     useEffect(()=>{
         searchMovies('Ironman');
